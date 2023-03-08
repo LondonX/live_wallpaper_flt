4e8268992c371815afae3d04f2be9f124304406a
LondonX <london@LondonX-iMac.local>
2023-03-08 17:49:52 +0800

    fix crash

--- android/src/main/kotlin/com/LondonX/live_wallpaper_flt/service/LiveWallpaperFltService.kt
+++ android/src/main/kotlin/com/LondonX/live_wallpaper_flt/service/LiveWallpaperFltService.kt
@@ -1,13 +1,14 @@
 package com.LondonX.live_wallpaper_flt.service
 
 import android.service.wallpaper.WallpaperService
-import android.util.Log
+import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.ViewConfiguration
 import com.LondonX.live_wallpaper_flt.entity.Config
 import com.google.gson.Gson
 import io.flutter.FlutterInjector
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterJNI
 import io.flutter.embedding.engine.dart.DartExecutor
 import io.flutter.embedding.engine.renderer.FlutterRenderer
 import kotlinx.coroutines.*
@@ -16,30 +17,42 @@ import java.io.File
 private const val TAG = "[live_wallpaper_flt]"
 
 class LiveWallpaperFltService : WallpaperService() {
-    private val config by lazy {
-        val json = File(filesDir, "live_wallpaper_config.json").readText()
-        Gson().fromJson(json, Config::class.java)
-            ?: throw Exception("${TAG}invalid config file, make sure you called LiveWallpaperFlt.instance.applyConfig first.")
-    }
-
-    private val flutterEngine by lazy {
-        FlutterEngine(this).apply {
-            dartExecutor.executeDartEntrypoint(
-                DartExecutor.DartEntrypoint(
-                    FlutterInjector.instance().flutterLoader().findAppBundlePath(),
-                    config.entryFunction,
-                )
-            )
-        }
-    }
 
     override fun onCreateEngine(): Engine {
         return object : Engine() {
+
+            private val config by lazy {
+                val json = File(filesDir, "live_wallpaper_config.json").readText()
+                Gson().fromJson(json, Config::class.java)
+                    ?: throw Exception("${TAG}invalid config file, make sure you called LiveWallpaperFlt.instance.applyConfig first.")
+            }
+
+            private val flutterEngine by lazy {
+                val newEngine = FlutterEngine(this@LiveWallpaperFltService)
+                newEngine.dartExecutor.executeDartEntrypoint(
+                    DartExecutor.DartEntrypoint(
+                        FlutterInjector.instance().flutterLoader().findAppBundlePath(),
+                        config.entryFunction,
+                    )
+                )
+                return@lazy newEngine
+            }
+
             private val scope = MainScope()
             private var refreshJob: Job? = null
 
-            override fun onCreate(surfaceHolder: SurfaceHolder?) {
-                super.onCreate(surfaceHolder)
+            private var width: Int? = null
+            private var height: Int? = null
+
+            private fun surfaceAttachScope(f: Surface.() -> Unit) {
+                val surface = surfaceHolder?.surface ?: return
+                val flutterJNI = FlutterRenderer::class.java.getDeclaredField("flutterJNI")
+                    .apply { isAccessible = true }
+                    .get(flutterEngine.renderer)
+                        as? FlutterJNI
+                if (flutterJNI?.isAttached == true) {
+                    f.invoke(surface)
+                }
             }
 
             override fun onVisibilityChanged(visible: Boolean) {
@@ -48,23 +61,29 @@ class LiveWallpaperFltService : WallpaperService() {
                     refreshJob = scope.launch {
                         while (true) {
                             delay(24)
-                            applyViewportMetrics()
+                            surfaceAttachScope {
+                                applyViewportMetrics()
+                            }
                         }
                     }
                     flutterEngine.lifecycleChannel.appIsResumed()
-                    flutterEngine.renderer.startRenderingToSurface(surfaceHolder.surface, true)
+                    surfaceAttachScope {
+                        flutterEngine.renderer.startRenderingToSurface(this, true)
+                    }
                 } else {
                     refreshJob?.cancel()
                     flutterEngine.lifecycleChannel.appIsPaused()
-                    flutterEngine.renderer.stopRenderingToSurface()
+                    surfaceAttachScope {
+                        flutterEngine.renderer.stopRenderingToSurface()
+                    }
                     flutterEngine.lifecycleChannel.appIsInactive()
                 }
             }
 
             override fun onDestroy() {
+                refreshJob?.cancel()
                 scope.cancel()
                 flutterEngine.lifecycleChannel.appIsDetached()
-                flutterEngine.renderer.stopRenderingToSurface()
                 flutterEngine.destroy()
                 super.onDestroy()
             }
@@ -73,22 +92,25 @@ class LiveWallpaperFltService : WallpaperService() {
                 holder: SurfaceHolder?, format: Int, width: Int, height: Int
             ) {
                 super.onSurfaceChanged(holder, format, width, height)
+                this.width = width
+                this.height = height
                 if (!flutterEngine.renderer.isDisplayingFlutterUi) return
-                applyViewportMetrics(width, height)
+                applyViewportMetrics()
             }
 
-            private fun applyViewportMetrics(
-                width: Int = resources.displayMetrics.widthPixels,
-                height: Int = resources.displayMetrics.heightPixels,
-            ) {
+            private fun applyViewportMetrics() {
+                val width = this.width ?: resources.displayMetrics.widthPixels
+                val height = this.height ?: resources.displayMetrics.heightPixels
                 val viewportMetrics = FlutterRenderer.ViewportMetrics()
                 viewportMetrics.devicePixelRatio = resources.displayMetrics.density
                 viewportMetrics.height = height
                 viewportMetrics.width = width
                 viewportMetrics.physicalTouchSlop =
                     ViewConfiguration.get(this@LiveWallpaperFltService).scaledTouchSlop
-                flutterEngine.renderer.setViewportMetrics(viewportMetrics)
-                flutterEngine.renderer.surfaceChanged(width, height)
+                surfaceAttachScope {
+                    flutterEngine.renderer.setViewportMetrics(viewportMetrics)
+                    flutterEngine.renderer.surfaceChanged(width, height)
+                }
             }
         }
     }
